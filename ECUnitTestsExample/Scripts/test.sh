71b780910b3168589e3e7ae3ef381578de22ab7a
Sam Deane <sam@elegantchaos.com>
2012-08-14 14:01:56 +0100

    fix for iOS unit tests

--- ECUnitTestsExample/Scripts/test.sh
+++ ECUnitTestsExample/Scripts/test.sh
@@ -18,10 +18,10 @@ echo Testing iOS ECUnitTests Examples
 #xcodebuild -workspace "ECUnitTestsExample.xcworkspace" -scheme "iOS Example" -sdk "iphonesimulator" -arch i386 build test ONLY_ACTIVE_ARCH=NO # | "$base/ocunit2junit.rb"
 
 cd ECUnitTests
-xcodebuild -project "ECUnitTests.xcodeproj" -target "ECUnitTestIOS" -arch i386 -sdk "iphonesimulator" build OBJROOT="$obj" SYMROOT="$sym"
+xcodebuild -project "ECUnitTests.xcodeproj" -target "ECUnitTestIOS" -arch i386 -sdk "iphonesimulator" clean build OBJROOT="$obj" SYMROOT="$sym"
 cd ../ECUnitTestsExample
-xcodebuild -project "ECUnitTestsExample.xcodeproj" -target "iOS Tests" -arch i386 -sdk "iphonesimulator" build OBJROOT="$obj" SYMROOT="$sym" TEST_AFTER_BUILD=YES
+xcodebuild -project "ECUnitTestsExample.xcodeproj" -target "iOS Tests" -arch i386 -sdk "iphonesimulator" clean build OBJROOT="$obj" SYMROOT="$sym" TEST_AFTER_BUILD=YES
 
-#rm -rf "$sym"
+rm -rf "$sym"
 
 
